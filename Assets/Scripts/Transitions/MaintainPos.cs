f703f6d6c541911f5e3351d6e9ba12849d8d6447
Cotawh <92425971+Cotawh@users.noreply.github.com>
2025-02-14 19:35:31 +0100

    anim transición v2
    Actualizada animación de transición +smooth

---
+++ Assets/Scripts/Transitions/MaintainPos.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class MaintainPos : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Vector3 posOriginal;
+    Vector3 offSet;
+    Vector3 newPos;
+    public GameObject parent;
+    void Start()
+    {
+        posOriginal = Vector3.zero;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        offSet = posOriginal + parent.transform.position;
+
+        if (parent.transform.position.x < 0)
+        {
+            newPos = new Vector3(-offSet.x, -offSet.y, 0f);
+            transform.position = parent.transform.position + newPos;
+        }
+        else
+        {
+            newPos = new Vector3(offSet.x, offSet.y, 0f);
+            transform.position = parent.transform.position-newPos;
+        }
+
+
+
+    }
+}
